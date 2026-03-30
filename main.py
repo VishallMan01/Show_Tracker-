ebccf2cdf98a47b650844bf46383d95834090c92
Vishall Manikaran <manik048@umn.edu>
2026-03-29 23:13:29 -0500

    frontend

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+shows: list[dict] = [
+
+{
+    "id": 1,
+    "name": "Attack on Titan",
+    "watch_status": "Watched",
+    "completeness": "Complete",
+    "review": "4 our of 5 stars I like the show"
+},
+{
+    "id": 2,
+    "name": "Naruto",
+    "watch_status": "Watched",
+    "completeness": "Complete",
+    "review": "Best show of my life"
+},
+{
+    "id": 2,
+    "name": "Solo Leveling",
+    "watch_status": "want to watch",
+    "completeness": "On-going",
+    "review": ""
+},
+]
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/shows")
+def root(request: Request):
+    return templates.TemplateResponse(
+        request, 
+        "home.html", 
+        {"shows": shows, "name": "Home"},
+    )
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/api/shows")
+def get_shows():
+    return shows
