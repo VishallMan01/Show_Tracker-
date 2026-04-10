02ce916bf9c4c460193a9b204a7e88abc4e5958b
Vishall Manikaran <manik048@umn.edu>
2026-04-09 23:18:07 -0500

    auth created for login

--- main.py
+++ main.py
@@ -90,6 +90,24 @@ async def user_post_page(request: Request, user_id: int, db: Annotated[AsyncSess
     )
 
 
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
+
+
 @app.exception_handler(StarletteHTTPException)
 async def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
 
